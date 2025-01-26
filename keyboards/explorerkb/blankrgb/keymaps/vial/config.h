22dbcd1f14f5577c8a321983e1d2fbdd1797b07f
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2025-01-27 01:35:27 +0800

    combine blank & blankrgb code

--- keyboards/explorerkb/blankrgb/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x31, 0xEC, 0x4C, 0x7D, 0xED, 0x29, 0x78, 0x09}
-#define VIAL_UNLOCK_COMBO_ROWS {0, 7}
-#define VIAL_UNLOCK_COMBO_COLS {1, 0}
