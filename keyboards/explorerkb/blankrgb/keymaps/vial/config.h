4daff51a250629ce8d6097e9bc6fca656c0d0ade
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2025-01-28 13:14:08 +0800

    rebuild blankrgb fw

---
+++ keyboards/explorerkb/blankrgb/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x6E, 0x90, 0xB2, 0x5D, 0x12, 0xD4, 0xF3, 0x1D}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 7}
+#define VIAL_UNLOCK_COMBO_COLS {1, 0}
